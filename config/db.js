2bcd901653fcf61a42f0b98a07122c07a5fa66c7
Michael <michaelkorostilev@gmail.com>
2023-01-22 15:48:34 +0200

    env url to string

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     mongoose.set("strictQuery", false);
-    const conn = await mongoose.connect(process.env.MONGO_URL, {
+    const conn = await mongoose.connect(`${process.env.MONGO_URL}`, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       //   useFindAndModify: false,
