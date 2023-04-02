30c8fd1683160bec9c52991049d6453f9256487c
Michael <michaelkorostilev@gmail.com>
2023-04-02 14:08:34 +0300

    added name to loginUser response controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -81,6 +81,7 @@ const loginUser = expressAsyncHandler(async (req, res) => {
   if (user && isMatch) {
     res.json({
       _id: user.id,
+      name: user.name,
       email: user.email,
       token: generateToken(user._id),
     });
