ca99b8c8e50dca9d3cd0eaa45924943cdb19c307
Michael <michaelkorostilev@gmail.com>
2023-04-05 18:19:44 +0300

    removed image update from username update

--- controllers/userController.js
+++ controllers/userController.js
@@ -98,7 +98,7 @@ const loginUser = expressAsyncHandler(async (req, res) => {
 // @access  Private
 
 const updateUserName = expressAsyncHandler(async (req, res) => {
-  const { name, image } = req.body;
+  const { name } = req.body;
 
   if (!name) {
     res.status(401);
@@ -108,7 +108,7 @@ const updateUserName = expressAsyncHandler(async (req, res) => {
   // find user by id and update
   const updatedUser = await User.findByIdAndUpdate(
     req.user._id,
-    { name, image },
+    { name },
     { new: true } // To return the updated user document
   );
 
@@ -119,7 +119,6 @@ const updateUserName = expressAsyncHandler(async (req, res) => {
 
   res.status(200).json({
     _id: updatedUser._id,
-    image: updatedUser.image,
     name: updatedUser.name,
     email: updatedUser.email,
     token: generateToken(updatedUser._id),
