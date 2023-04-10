f5a8251dd20a9f589c64d3c6e1e62e66a5cfe454
Michael <michaelkorostilev@gmail.com>
2023-04-10 13:21:26 +0300

    updated corsMiddleware

--- middleware/corsMiddleware.js
+++ middleware/corsMiddleware.js
@@ -1,5 +1,5 @@
 const cors = (req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Origin", req.headers.origin || "*");
   res.setHeader(
     "Access-Control-Allow-Methods",
     "GET, POST, OPTIONS, PUT, PATCH, DELETE"
@@ -9,10 +9,25 @@ const cors = (req, res, next) => {
     "X-Requested-With,Content-Type,Authorization"
   );
   res.setHeader("Access-Control-Allow-Credentials", true);
-  res.setHeader("Cache-Control", "no-cache");
+
   // Pass to next layer of middleware
-  if (req.method === "OPTIONS") res.sendStatus(200);
-  else next();
+  if (req.method === "OPTIONS") {
+    res.setHeader("Access-Control-Allow-Origin", req.headers.origin || "*");
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+    );
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "X-Requested-With,Content-Type,Authorization"
+    );
+    res.setHeader("Access-Control-Allow-Credentials", true);
+    res.setHeader("Access-Control-Max-Age", "86400");
+    res.sendStatus(200);
+  } else next();
 };
 
 module.exports = { cors };
+
+// if (req.method === "OPTIONS") res.sendStatus(200);
+//   res.setHeader("Cache-Control", "no-cache");
