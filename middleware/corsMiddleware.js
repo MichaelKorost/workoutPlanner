026c06d5eda7f4cee4ba700bad5da905c8757264
Michael <michaelkorostilev@gmail.com>
2023-04-10 13:34:56 +0300

    removed unnecessary changes to cors

--- middleware/corsMiddleware.js
+++ middleware/corsMiddleware.js
@@ -11,23 +11,10 @@ const cors = (req, res, next) => {
   res.setHeader("Access-Control-Allow-Credentials", true);
 
   // Pass to next layer of middleware
-  if (req.method === "OPTIONS") {
-    res.setHeader("Access-Control-Allow-Origin", req.headers.origin || "*");
-    res.setHeader(
-      "Access-Control-Allow-Methods",
-      "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-    );
-    res.setHeader(
-      "Access-Control-Allow-Headers",
-      "X-Requested-With,Content-Type,Authorization"
-    );
-    res.setHeader("Access-Control-Allow-Credentials", true);
-    res.setHeader("Access-Control-Max-Age", "86400");
-    res.sendStatus(200);
-  } else next();
+  if (req.method === "OPTIONS") res.sendStatus(200);
+  else next();
 };
 
 module.exports = { cors };
 
-// if (req.method === "OPTIONS") res.sendStatus(200);
 //   res.setHeader("Cache-Control", "no-cache");
