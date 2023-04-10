2356baf5f4bfa749acacd4b55c0c807bba429c31
Michael <michaelkorostilev@gmail.com>
2023-04-10 13:31:30 +0300

    providing limit for http requests

--- index.js
+++ index.js
@@ -12,8 +12,10 @@ const app = express();
 
 app.use(cors);
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json({ limit: "50mb" }));
+app.use(
+  express.urlencoded({ limit: "50mb", extended: true, parameterLimit: 50000 })
+);
 
 app.use("/api/workouts", require("./routes/workoutRoutes"));
 app.use("/api/exercises", require("./routes/exerciseRoutes"));
